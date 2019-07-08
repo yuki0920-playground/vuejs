bfc5695c1e5b067a9d499d0da29dd641bde5fc8a
yuki0920 <ywsep20@gmail.com>
2019-07-09 07:08:16 +0900

    オブジェクトの繰り返しv-for

--- section2/js/main.js
+++ section2/js/main.js
@@ -1,6 +1,11 @@
 var app = new Vue ({
 	el: '#app',
 	data: {
-		colors: ['Red', 'Green', 'Blue']
+		// userオブジェクト(複数の属性/プロパティ)を持ったモノ
+		user: {
+			firstName: 'Taro',
+			lastName: 'Yamada',
+			age: 28
+		}
 	}
 })
