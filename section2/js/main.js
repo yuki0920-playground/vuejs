ba1fbbbbd2915c07e0a752a49dfc9fc20b41ddf0
yuki0920 <ywsep20@gmail.com>
2019-07-09 07:14:46 +0900

    イベント処理の基本v-on

--- section2/js/main.js
+++ section2/js/main.js
@@ -1,11 +1,12 @@
 var app = new Vue ({
 	el: '#app',
 	data: {
-		// userオブジェクト(複数の属性/プロパティ)を持ったモノ
-		user: {
-			firstName: 'Taro',
-			lastName: 'Yamada',
-			age: 28
+		now: ''
+	},
+	methods: {
+		onclick: function(){
+			// alert('onclick!');
+			this.now = new Date().toLocaleString();
 		}
 	}
 })
