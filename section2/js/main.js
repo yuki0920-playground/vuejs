d68cdbffb6e7b32add0819971b1e14ecc418ff5b
yuki0920 <ywsep20@gmail.com>
2019-07-08 21:50:43 +0900

    ルートテンプレート

--- section2/js/main.js
+++ section2/js/main.js
@@ -1,13 +1,3 @@
-var app = new Vue ( {
+var app = new Vue ({
 	el: '#app',
-	data : {
-		message: 'Hello Vue js!',
-    count: 10,
-    user: {
-     lastName: 'Yamada',
-     firstName: 'Taro',
-     prefecture: 'Tokyo'
-    },
-    colors: ['Red', 'Green', 'Blue']
-	}
-})
+	})
