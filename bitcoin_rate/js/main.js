cc15227e56f7187dcaa04b3ff103d7de580d8d03
yuki0920 <ywsep20@gmail.com>
2019-07-11 20:52:45 +0900

    通信エラーのキャッチ

--- bitcoin_rate/js/main.js
+++ bitcoin_rate/js/main.js
@@ -7,7 +7,12 @@ var app = new Vue({
   mounted: function() {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then(function(response){
-      console.log(response)
+      // console.log(response.data.bpi)
+      // console.log(response.data.bpi.USD.rate_float)
+      this.bpi = response.data.bpi
+    }.bind(this))
+    .catch(function(erro){
+      console.log(erro)
     })
   }
 })
