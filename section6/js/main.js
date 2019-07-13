e35a81940801e7e98b651b6c5da4cc6198b8ecf0
yuki0920 <ywsep20@gmail.com>
2019-07-13 21:20:37 +0900

    算出プロパティの基本

---
+++ section6/js/main.js
@@ -0,0 +1,12 @@
+var app = new Vue ( {
+	el: '#app',
+	data: {
+		message: 'Hello Vue.js!'
+	},
+	// 複雑な処理をjsに切り出すためのプロパティ
+	computed: {
+		reversedMessage: function(){
+			return this.message.split('').reverse().join('')
+		}
+	}
+})
