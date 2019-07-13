54d7499547854ae975267805f3c2fe4db37b8087
yuki0920 <ywsep20@gmail.com>
2019-07-13 15:00:25 +0900

    グローバルフィルター

--- section5/js/main.js
+++ section5/js/main.js
@@ -1,11 +1,16 @@
+Vue.filter('numberFormat', function(value) {
+	return value.toLocaleString()
+}
+)
+
 var app = new Vue ({
 	el: "#app",
 	data: {
 		price: 29800000,
 	},
-	filters: {
-		numberFormat: function(value){
-			return value.toLocaleString()
-		}
-	}
+	// filters: {
+	// 	numberFormat: function(value){
+	// 		return value.toLocaleString()
+	// 	}
+	// }
 })
