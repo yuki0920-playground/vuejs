f9cdaee1f9c2beb29361a73007421a7c2d6c73f9
yuki0920 <ywsep20@gmail.com>
2019-07-13 15:46:43 +0900

    フィルタの引数

--- section5/js/main.js
+++ section5/js/main.js
@@ -1,14 +1,10 @@
-Vue.filter('toUSD', function(jpy){
-	return jpy / 100
-})
-
-Vue.filter('numberFormat', function(value){
-	return value.toLocaleString()
+Vue.filter('readMore', function(text, length, suffix){
+	return text.substring(0, length) + suffix
 })
 
 var app = new Vue ({
 	el: "#app",
 	data: {
-		jpyPrice: 298000
+		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. '
 	}
 })
