a41feabf173773a1589484ad9f00b0cc2efd2fc5
yuki0920 <ywsep20@gmail.com>
2019-07-13 15:50:24 +0900

    v-bind省略記法

--- section5/js/main.js
+++ section5/js/main.js
@@ -1,10 +1,6 @@
-Vue.filter('readMore', function(text, length, suffix){
-	return text.substring(0, length) + suffix
-})
-
 var app = new Vue ({
 	el: "#app",
 	data: {
-		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. '
+		url: 'https://www.google.com'
 	}
 })
