81691d91aa8bf1a097c6ec6b241c964e0243db71
yuki0920 <ywsep20@gmail.com>
2019-07-14 09:20:59 +0900

    監視プロパティ > 単位変換アプリ

--- section7/js/main.js
+++ section7/js/main.js
@@ -1,11 +1,28 @@
+
+
 var app = new Vue ( {
 	el: '#app',
 	data: {
-		message: 'Hello Vue.js!'
+		km: 0,
+		m: 0,
+		mm: 0
 	},
 	watch: {
-		message: function(newValue, oldValue){
-			console.log('new %s, old: %s', newValue, oldValue )
+		km: function(value){
+			// console.log(value)
+			this.km = value
+			this.m = value * 1000
+			this.mm = value * 1000000
+		},
+		m: function(value){
+			this.km = value / 1000
+			this.m = value
+			this.mm = value * 1000
+		},
+		mm: function(value){
+			this.km = value / 1000000
+			this.m = value / 1000
+			this.mm = value
 		}
 	}
 })
