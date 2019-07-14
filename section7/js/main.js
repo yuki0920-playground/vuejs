cfdc997f0f770c246449ece27e3ad62a9188b4e6
yuki0920 <ywsep20@gmail.com>
2019-07-14 09:55:08 +0900

    オプションdeep2

--- section7/js/main.js
+++ section7/js/main.js
@@ -10,7 +10,11 @@ var app = new Vue ( {
 	 watch: {
 		 colors: {
 			 handler: function(newValue, oldValue){
-				 console.log('Update')
+				 console.log('Update'),
+				//  deeオプションは古い値が新しい値に書き換えられる仕様
+				 console.log('new %s, oldValue %s',
+					JSON.stringify(newValue, null, '\t'),
+					JSON.stringify(oldValue, null, '\t'))
 			 },
 			//  配列colors配下のcolorが変更されるたび呼び出される
 			 deep: true
