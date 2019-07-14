ba8bc68ae0fb947b268115c17373539b59c0013a
yuki0920 <ywsep20@gmail.com>
2019-07-14 09:41:40 +0900

    監視プロパティと算出プロパティ

--- section7/js/main.js
+++ section7/js/main.js
@@ -1,28 +1,29 @@
-
+// var app = new Vue ( {
+// 	el: '#app',
+// 	data: {
+// 		firstName:'',
+// 		lastName:'',
+// 		fullName:''
+// 	},
+// 	watch: {
+// 		firstName: function(value){
+// 			this.fullName = value + ' ' + this.lastName
+// 		},
+// 		lastName: function(value){
+// 			this.fullName = this.firstName + ' ' + value
+// 		}
+// 	}
+// })
 
 var app = new Vue ( {
 	el: '#app',
 	data: {
-		km: 0,
-		m: 0,
-		mm: 0
+		firstName:'',
+		lastName:'',
 	},
-	watch: {
-		km: function(value){
-			// console.log(value)
-			this.km = value
-			this.m = value * 1000
-			this.mm = value * 1000000
-		},
-		m: function(value){
-			this.km = value / 1000
-			this.m = value
-			this.mm = value * 1000
-		},
-		mm: function(value){
-			this.km = value / 1000000
-			this.m = value / 1000
-			this.mm = value
+	computed: {
+		fullName: function(){
+			return this.firstName + " " + this.lastName
 		}
 	}
 })
