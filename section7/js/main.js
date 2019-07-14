7628f1a6c0259886c0f0d3abe7a9d49be16ef071
yuki0920 <ywsep20@gmail.com>
2019-07-14 09:49:16 +0900

    オプションdeep1

--- section7/js/main.js
+++ section7/js/main.js
@@ -1,29 +1,19 @@
-// var app = new Vue ( {
-// 	el: '#app',
-// 	data: {
-// 		firstName:'',
-// 		lastName:'',
-// 		fullName:''
-// 	},
-// 	watch: {
-// 		firstName: function(value){
-// 			this.fullName = value + ' ' + this.lastName
-// 		},
-// 		lastName: function(value){
-// 			this.fullName = this.firstName + ' ' + value
-// 		}
-// 	}
-// })
-
 var app = new Vue ( {
 	el: '#app',
 	data: {
-		firstName:'',
-		lastName:'',
-	},
-	computed: {
-		fullName: function(){
-			return this.firstName + " " + this.lastName
-		}
-	}
+		colors: [
+			{ name: 'Red' },
+			{ name: 'Green' },
+			{ name: 'Blue' },
+		]
+	 },
+	 watch: {
+		 colors: {
+			 handler: function(newValue, oldValue){
+				 console.log('Update')
+			 },
+			//  配列colors配下のcolorが変更されるたび呼び出される
+			 deep: true
+		 }
+	 }
 })
