15015ad88bc3f715152b01661fd60b5994905161
yuki0920 <ywsep20@gmail.com>
2019-07-16 07:05:17 +0900

    APIからデータを取得

--- template8/js/main.js
+++ template8/js/main.js
@@ -3,15 +3,35 @@ var app = new Vue ( {
 	data: {
 		items: null,
 		keyword: '',
-		message: '',
+		message: 'hello vue.js',
 	},
 	watch: {
-
 	},
-	created: {
-
+	created: function(){
+		this.keyword = 'JavaScript'
+		this.getAnswer()
 	},
 	methods: {
-
+		getAnswer: function(){
+			if(this.keyword === ''){
+				this.items = null
+				this.message = ''
+				return
+			}
+			this.message = 'Loading...'
+			var vm = this
+			var params = { page: 1, per_page: 20, query: this.keyword }
+			axios.get('https://qiita.com/api/v2/items', { params })
+			.then(function(response){
+				console.log(response)
+				vm.items = response.data
+			})
+			.catch(function(error) {
+				vm.message = 'Error!' + error
+			})
+			.finally(function() {
+				vm.message = ''
+			})
+		}
 	}
 })
