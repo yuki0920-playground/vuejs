a793ac9e80dc03eed56b8e411922c396dca750e1
yuki0920 <ywsep20@gmail.com>
2019-07-17 08:35:34 +0900

    インラインスタイルのデータバインディング

--- section9/js/main.js
+++ section9/js/main.js
@@ -1,13 +1,7 @@
 var app = new Vue ( {
 	el: '#app',
 	data: {
-		largeClass: {
-			large: true,
-			'bg-gray': true
-		},
-		dangerClass: {
-			'text-danger':true
-		},
-		isLarge: true
+		color: 'red',
+		fontSize: 48,
 	}
 })
