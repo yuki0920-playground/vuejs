fa12f93f2e240ea1bcfe6e6e3b1821c118359fd3
yuki0920 <ywsep20@gmail.com>
2019-07-17 10:20:52 +0900

    イベント修飾子once

--- section11/js/main.js
+++ section11/js/main.js
@@ -1,15 +1,11 @@
 var app = new Vue ( {
 	el: '#app',
 	data: {
-		counter: 0
+		message: ''
 	},
 	methods: {
-		clickHandler: function(event){
-			this.counter++
-			console.log(event)
-			console.log(event.target.tagName)
-			console.log(event.target.innerHTML)
-			console.log(event.target.type)
+		clickHandler: function(){
+				this.message = new Date().toLocaleTimeString()
 		}
 	}
 })
