f04d718c2d4db795c875c56757ac7461f0c0126c
yuki0920 <ywsep20@gmail.com>
2019-07-18 19:14:20 +0900

    コンポーネントの定義 ローカル登録

--- section13/js/main.js
+++ section13/js/main.js
@@ -1,8 +1,16 @@
 // 第1引数にコンポーネント名、第2引数にテンプレート
-Vue.component('hello-component', {
+// Vue.component('hello-component', {
+// 	template: '<p>Hello</p>'
+// })
+
+// 変数にテンプレートを代入し、componentプロパティで呼び出す
+var helloComponent = {
 	template: '<p>Hello</p>'
-})
+}
 
 var app = new Vue ( {
 	el: '#app',
+	components: {
+		'hello-component': helloComponent
+	}
 })
